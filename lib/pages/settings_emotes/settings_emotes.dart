0eb1fcae5f0c63c73ba2ac9a8889e2fd9fcc37cc
Sonth <huysonto@gmail.com>
2025-10-20 10:02:39 +0700

    update lib

--- lib/pages/settings_emotes/settings_emotes.dart
+++ lib/pages/settings_emotes/settings_emotes.dart
@@ -286,9 +286,9 @@ class EmotesSettingsController extends State<EmotesSettings> {
 
         if (result.isEmpty) return null;
 
-        final buffer = InputStream(await result.first.readAsBytes());
+        final buffer = InputMemoryStream(await result.first.readAsBytes());
 
-        final archive = ZipDecoder().decodeBuffer(buffer);
+        final archive = ZipDecoder().decodeStream(buffer);
 
         return archive;
       },
